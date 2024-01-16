e49e30e9eeb787ca22cd494c792b10f301b23ac0
Sai Manasa <tsaisatyapriya@gmail.com>
2024-01-16 15:56:33 +0530

    added pythonfile with definitions

--- model.py
+++ model.py
@@ -1,3 +1,12 @@
 model.py
 
-print("Model Python for new branch)
+print("Model Python for new branch")
+
+def model1():
+    print("adding code for model1")
+
+def model2():
+    print("adding code for model2")
+
+def model3():
+    print("adding code for model3")
